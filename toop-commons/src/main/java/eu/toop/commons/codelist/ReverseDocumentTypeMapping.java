a5840ff9dd449009706f44dff5bf07d417021ff9
Philip Helger <philip@helger.com>
2018-05-16 10:39:16 +0200

    Added reverse mapping

---
+++ toop-commons/src/main/java/eu/toop/commons/codelist/ReverseDocumentTypeMapping.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2018 toop.eu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.toop.commons.codelist;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.collection.impl.CommonsHashMap;
+import com.helger.commons.collection.impl.ICommonsMap;
+
+/**
+ * This class manages the relationship between TOOP request and response
+ * Document types, so that .
+ *
+ * @author Philip Helger
+ */
+public final class ReverseDocumentTypeMapping {
+  private static final ICommonsMap<EPredefinedDocumentTypeIdentifier, EPredefinedDocumentTypeIdentifier> s_aMap = new CommonsHashMap<> ();
+
+  private static void _add (@Nonnull final EPredefinedDocumentTypeIdentifier aRequest,
+                            @Nonnull final EPredefinedDocumentTypeIdentifier aResponse) {
+    s_aMap.put (aRequest, aResponse);
+    s_aMap.put (aResponse, aRequest);
+  }
+
+  static {
+    // Fill the map
+    _add (EPredefinedDocumentTypeIdentifier.REQUEST_REGISTEREDORGANIZATION,
+          EPredefinedDocumentTypeIdentifier.RESPONSE_REGISTEREDORGANIZATION);
+  }
+
+  private ReverseDocumentTypeMapping () {
+  }
+
+  @Nullable
+  public static EPredefinedDocumentTypeIdentifier getReverseDocumentTypeOrNull (@Nullable final EPredefinedDocumentTypeIdentifier eDocType) {
+    return eDocType == null ? null : s_aMap.get (eDocType);
+  }
+
+  @Nonnull
+  @Nonempty
+  public static EPredefinedDocumentTypeIdentifier getReverseDocumentType (@Nullable final EPredefinedDocumentTypeIdentifier eDocType) {
+    final EPredefinedDocumentTypeIdentifier ret = getReverseDocumentTypeOrNull (eDocType);
+    if (ret == null)
+      throw new IllegalArgumentException ("Unsupported document type " + eDocType);
+    return ret;
+  }
+}
