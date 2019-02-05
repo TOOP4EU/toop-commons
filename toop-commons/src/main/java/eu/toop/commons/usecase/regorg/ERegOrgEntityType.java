92bcfb1c4fd290bbe141a48cf1d778a2ccf24788
Philip Helger <philip@helger.com>
2019-02-05 16:19:44 +0100

    Alphabetical order

--- toop-commons/src/main/java/eu/toop/commons/usecase/regorg/ERegOrgEntityType.java
+++ toop-commons/src/main/java/eu/toop/commons/usecase/regorg/ERegOrgEntityType.java
@@ -2,11 +2,11 @@ package eu.toop.commons.usecase.regorg;
 
 /**
  * Difference between natural person and legal entity.
- * 
+ *
  * @author Philip Helger
  */
 public enum ERegOrgEntityType
 {
-  NATURAL_PERSON,
-  LEGAL_ENTITY;
+  LEGAL_ENTITY,
+  NATURAL_PERSON;
 }
