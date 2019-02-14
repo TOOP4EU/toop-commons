9f0595fd205112e382aec95fc930f89d3ed51347
Philip Helger <philip@helger.com>
2019-02-14 14:34:42 +0100

    Extended API

--- toop-commons/src/main/java/eu/toop/commons/usecase/regorg/ERegOrgEntityType.java
+++ toop-commons/src/main/java/eu/toop/commons/usecase/regorg/ERegOrgEntityType.java
@@ -1,12 +1,40 @@
 package eu.toop.commons.usecase.regorg;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.id.IHasID;
+import com.helger.commons.lang.EnumHelper;
+
 /**
- * Difference between natural person and legal entity.
+ * Difference between legal entity and ERegOrgEntityType.
  *
  * @author Philip Helger
+ * @since 0.10.0
  */
-public enum ERegOrgEntityType
+public enum ERegOrgEntityType implements IHasID <String>
 {
-  LEGAL_ENTITY,
-  NATURAL_PERSON;
+  LEGAL_ENTITY ("LE"),
+  NATURAL_PERSON ("NP");
+
+  private final String m_sID;
+
+  private ERegOrgEntityType (@Nonnull @Nonempty final String sID)
+  {
+    m_sID = sID;
+  }
+
+  @Nonnull
+  @Nonempty
+  public String getID ()
+  {
+    return m_sID;
+  }
+
+  @Nullable
+  public static ERegOrgEntityType getFromIDOrNull (@Nullable final String sID)
+  {
+    return EnumHelper.getFromIDOrNull (ERegOrgEntityType.class, sID);
+  }
 }
