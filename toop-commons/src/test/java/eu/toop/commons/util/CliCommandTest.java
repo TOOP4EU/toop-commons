e6cfa91a9be371a25492036f26a9183a3ae37a8e
Philip Helger <philip@helger.com>
2019-10-17 21:26:08 +0200

    No JUnit 5

--- toop-commons/src/test/java/eu/toop/commons/util/CliCommandTest.java
+++ toop-commons/src/test/java/eu/toop/commons/util/CliCommandTest.java
@@ -15,52 +15,60 @@
  */
 package eu.toop.commons.util;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-public class CliCommandTest {
+import com.helger.commons.collection.impl.CommonsArrayList;
+import com.helger.commons.regex.RegExHelper;
+
+public class CliCommandTest
+{
   @Test
-  public void parseEmptyCommandList() {
-    try {
-      CliCommand.parse(new ArrayList<>(), true);
-      fail();
-    } catch (IllegalArgumentException ex) {
+  public void parseEmptyCommandList ()
+  {
+    try
+    {
+      CliCommand.parse (new ArrayList <> (), true);
+      fail ();
+    }
+    catch (final IllegalArgumentException ex)
+    {
       // Expected
     }
   }
 
   @Test
-  public void parseOneCommand() {
-    CliCommand singleCommand = CliCommand.parse(Arrays.asList("singleCommand"), true);
+  public void parseOneCommand ()
+  {
+    final CliCommand singleCommand = CliCommand.parse (Arrays.asList ("singleCommand"), true);
 
-    assertEquals("singleCommand", singleCommand.getMainCommand());
-    assertNull(singleCommand.getOptions());
+    assertEquals ("singleCommand", singleCommand.getMainCommand ());
+    assertNull (singleCommand.getOptions ());
   }
 
   @Test
-  public void parseComplexCommand() throws Exception {
-    String s = "sampleCommand optionwithoutdash1 optionwithoutdash2 -f file1 -q -t option1 option2 -c option3";
-    final Method split = String.class.getMethod("split", String.class);
-    String[] tokens = (String[]) split.invoke(s, "\\s");
-    CliCommand singleCommand = CliCommand.parse(Arrays.asList(tokens), true);
+  public void parseComplexCommand () throws Exception
+  {
+    final String s = "sampleCommand optionwithoutdash1 optionwithoutdash2 -f file1 -q -t option1 option2 -c option3";
+    final String [] tokens = RegExHelper.getSplitToArray (s, "\\s");
+    final CliCommand singleCommand = CliCommand.parse (new CommonsArrayList <> (tokens), true);
 
-    assertEquals("sampleCommand", singleCommand.getMainCommand());
-    assertNotNull(singleCommand.getOptions());
+    assertEquals ("sampleCommand", singleCommand.getMainCommand ());
+    assertNotNull (singleCommand.getOptions ());
 
-    assertArrayEquals(new String[]{"optionwithoutdash1", "optionwithoutdash2"},
-        singleCommand.getEmptyParameters().toArray());
-    assertArrayEquals(new String[]{"file1"}, singleCommand.getArguments("f").toArray());
-    assertArrayEquals(new String[0], singleCommand.getArguments("q").toArray());
-    assertArrayEquals(new String[]{"option1", "option2"}, singleCommand.getArguments("t").toArray());
-    assertArrayEquals(new String[]{"option3"}, singleCommand.getArguments("c").toArray());
+    assertArrayEquals (new String [] { "optionwithoutdash1", "optionwithoutdash2" },
+                       singleCommand.getEmptyParameters ().toArray ());
+    assertArrayEquals (new String [] { "file1" }, singleCommand.getArguments ("f").toArray ());
+    assertArrayEquals (new String [0], singleCommand.getArguments ("q").toArray ());
+    assertArrayEquals (new String [] { "option1", "option2" }, singleCommand.getArguments ("t").toArray ());
+    assertArrayEquals (new String [] { "option3" }, singleCommand.getArguments ("c").toArray ());
   }
 }
