faaa36853d3ff159ffa4df6e931f901b9646c949
Philip Helger <philip@helger.com>
2020-03-04 22:04:13 +0100

    Updated license headers

--- toop-codelist-tools/src/main/java/eu/toop/codelist/tools/AbstractMain.java
+++ toop-codelist-tools/src/main/java/eu/toop/codelist/tools/AbstractMain.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2018-2020 toop.eu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package eu.toop.codelist.tools;
 
 import javax.annotation.Nonnull;
@@ -13,7 +28,8 @@ abstract class AbstractMain
 {
   public static final Version CODELIST_VERSION = new Version (2, 0, 0);
   public static final String DO_NOT_EDIT = "This file was automatically generated.\nDo NOT edit!";
-  private static final String CL_XML_DIRECTORY = "../../toop/Code Lists";
+  public static final String CODELIST_XLSX_DIR = "../../toop/Code Lists/";
+  private static final String CL_XML_DIRECTORY = CODELIST_XLSX_DIR;
 
   protected AbstractMain ()
   {}
--- toop-codelist-tools/src/main/java/eu/toop/codelist/tools/MainCreateCodeListsFromExcel.java
+++ toop-codelist-tools/src/main/java/eu/toop/codelist/tools/MainCreateCodeListsFromExcel.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2018-2020 toop.eu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package eu.toop.codelist.tools;
 
 import java.io.File;
@@ -267,7 +282,8 @@ public final class MainCreateCodeListsFromExcel extends AbstractMain
     public CodeListFile (@Nonnull final String sFilenamePart,
                          @Nonnull final IThrowingConsumer <? super Sheet, Exception> aHandler)
     {
-      m_aFile = new File ("../Toop" +
+      m_aFile = new File (CODELIST_XLSX_DIR +
+                          "Toop" +
                           sFilenamePart +
                           "-v" +
                           CODELIST_VERSION.getAsString (false) +
